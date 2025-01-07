673e078adf9a297a1b934532d7ca7110409b6a97
Samuel <smonux@gmail.com>
2025-01-07 09:50:33 +0000

    several fixes

--- datasources.py
+++ datasources.py
@@ -1,7 +1,17 @@
+"""
+title: Apache Drill SQL Tool
+author: smonux
+version: 0.0.1
+
+This code allows an llm to explore and run SQL on an Apache Drill instance (without authentication).
+
+"""
+
 import requests
 import yaml
 from pydantic import BaseModel, Field
 from typing import Callable, Awaitable
+import datetime
 
 event_data_template = """
 ---
@@ -41,7 +51,7 @@ def printTable(myDict, colList=None):
     """Pretty print a list of dictionaries (myDict) as a dynamically sized table.
     If column names (colList) aren't specified, they will show in random order.
     Author: Thierry Husson - Use it as you want but don't blame me.
-    https://stackoverflow.com/questions/17330139/python-printing-a-dictionary-as-a-horizontal-table-with-headers
+    source: https://stackoverflow.com/questions/17330139/python-printing-a-dictionary-as-a-horizontal-table-with-headers
     """
     colList = list(myDict[0].keys() if myDict else [])
     myList = [colList]  # 1st row = header
@@ -61,24 +71,14 @@ def printTable(myDict, colList=None):
 class Tools:
     class Valves(BaseModel):
         DRILL_URL: str = Field(
-            default="http://localhost:8047",
+            default="http://drill:8047",
             description="The URL of the Apache Drill instance.",
         )
         MAX_ROWS: int = Field(default=100, description="Maximum rows returned")
 
     def __init__(self):
         self.valves = self.Valves()
-        self.drill_url = self.valves.DRILL_URL
-        self.max_rows = self.valves.MAX_ROWS
-
-    async def get_structure(
-        self, __event_emitter__: Callable[[dict], Awaitable[None]]
-    ) -> str:
-        """Get the definition of available datasources"""
-        sql_columns = "SELECT c.* FROM information_schema.columns c"
-        return await self.execute_sql(
-            query=sql_columns, __event_emitter__=__event_emitter__
-        )
+        self.citation = True
 
     async def list_databases(
         self, __event_emitter__: Callable[[dict], Awaitable[None]]
@@ -90,7 +90,9 @@ class Tools:
     async def list_tables(
         self, database: str, __event_emitter__: Callable[[dict], Awaitable[None]]
     ) -> str:
-        """List all tables in a specified database"""
+        """List all tables in a specified database
+        :param database: The database to get the table list
+        """
         query = f"SHOW TABLES IN `{database}`"
         return await self.execute_sql(query=query, __event_emitter__=__event_emitter__)
 
@@ -100,19 +102,11 @@ class Tools:
         table: str,
         __event_emitter__: Callable[[dict], Awaitable[None]],
     ) -> str:
-        """Describe the schema of a specified table"""
+        """Describe the schema of a specified table
+        :param database: the drill database that contains the specific table
+        :param table: the table whose metadata is needed
+        """
         query = f"DESCRIBE `{database}`.`{table}`"
-        return await self.execute_sql(query=query, __event_emitter__=__event_emitter__)
-
-    async def get_sample_rows(
-        self,
-        database: str,
-        table: str,
-        limit: int,
-        __event_emitter__: Callable[[dict], Awaitable[None]],
-    ) -> str:
-        """Retrieve sample rows from a specified table"""
-        query = f"SELECT * FROM `{database}`.`{table}` LIMIT {limit}"
         return await self.execute_sql(query=query, __event_emitter__=__event_emitter__)
 
     async def get_table_metadata(
@@ -133,30 +127,67 @@ class Tools:
     async def execute_sql(
         self, query: str, __event_emitter__: Callable[[dict], Awaitable[None]]
     ) -> str:
-        """Executes a given SQL query on Apache Drill."""
-        url = f"{self.drill_url}/query.json"
-        payload = {"queryType": "SQL", "query": query, "autoLimit": self.max_rows}
+        """
+        Executes a given SQL query on Apache Drill.
+        :param query: query to run
+        """
+        url = f"{self.valves.DRILL_URL}/query.json"
+        if query.endswith(";"):
+            query = query[:-1]
+        payload = {
+            "queryType": "SQL",
+            "query": query,
+            # This makes most databases fail because of the syntax it uses
+            # "autoLimit": self.valves.MAX_ROWS,
+            "options": {"drill.exec.http.rest.errors.verbose": True},
+        }
         try:
             response = requests.post(url, json=payload)
             response.raise_for_status()
             json_data = response.json()
             table = ""
+            output = ""
+
             if "rows" in json_data:
-                table = printTable(json_data["rows"], json_data.get("columns", None))
+                if len(json_data["rows"]) > self.valves.MAX_ROWS:
+                    output += f"Data truncated to {self.valves.MAX_ROWS} Rows!!!\n"
+                table = printTable(
+                    json_data["rows"][: self.valves.MAX_ROWS],
+                    json_data.get("columns", None),
+                )
                 del json_data["rows"]
-            output = yaml.dump(json_data, default_flow_style=False)
+
+            # drill.exec.http.rest.errors.verbose is needed for good error description
+            # but the stackTrace is too verbose and not necessary
+            if "stackTrace" in json_data:
+                del json_data["stackTrace"]
+            output += yaml.dump(json_data, default_flow_style=False)
             if table:
                 output += "\n--ROWS----\n\n" + table
         except requests.exceptions.RequestException as e:
             output = f"Error: {str(e)}"
-
+        await __event_emitter__(
+            {
+                "type": "message",
+                "data": {
+                    "content": event_data_template.format(
+                        query=query,
+                        output=output,
+                        ts=datetime.datetime.now().isoformat(),
+                    )
+                },
+            }
+        )
         return sql_output_template.format(output=output, query=query)
 
     async def explain_plan_sql(
         self, query: str, __event_emitter__: Callable[[dict], Awaitable[None]]
     ) -> str:
-        """Executes an EXPLAIN PLAN for a given SQL query on Apache Drill."""
+        """Executes an EXPLAIN PLAN for a given SQL query on Apache Drill.
+        :param query: query to get explain plan from
+        """
         explain_query = f"EXPLAIN PLAN FOR {query}"
         return await self.execute_sql(
             query=explain_query, __event_emitter__=__event_emitter__
         )
+
