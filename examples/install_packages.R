eff7fb1e234d426f1b3d2bc826e47c200343159d
Samuel <smonux@gmail.com>
2025-01-07 10:47:47 +0000

    file reorg

---
+++ examples/install_packages.R
@@ -0,0 +1,10 @@
+#require(devtools)
+#install_version("dbplyr", "2.4.0")
+install.packages(c(
+  "gridExtra",
+  "sergeant",
+  "rpart",
+  "rpart.plot",
+  "caret",
+  "randomForest"
+))
--- install_packages.R
+++
@@ -1,4 +0,0 @@
-install.packages(c(
-  "gridExtra",
-  "sergeant"
-))
